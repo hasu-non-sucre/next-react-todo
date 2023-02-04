77a38e5508628816388fb0285f1466cc1b59eea6
Muto Sota <hasu_m.s@icloud.com>
2023-02-04 16:36:48 +0900

    add: メインの追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,34 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  function TodoItem({ item, onCheck }) {
+    const handleChange = () => {
+      onCheck(item);
+    }
+
+    return (
+      <label className='panel-block'>
+        <input
+          type='checkbox'
+          checked={item.done}
+          onChange={handleChange}
+        />
+        <span className={classNames({
+          'has-text-grey-light': item.done
+        })}
+        >
+          {item.text}
+        </span>
+      </label>
+    )
+  }
 
   function Todo() {
     const getKey = () => Math.random().toString(32).substring(2);
@@ -16,23 +39,114 @@ export default function Home() {
       { key: getKey(), text: 'Get some good sleep', done: false },
     ]);
 
+    const handleCheck = checked => {
+      const newItems = items.map(item => {
+        if (item.key === checked.key) {
+          item.done = !item.done;
+        }
+        return item;
+      });
+      setItems(newItems);
+    };
+
+    const handleAdd = text => {
+      setItems([...items, {key: getKey(), text, done: false}])
+    };
+
+    const [filter, setFilter] = React.useState('ALL');
+
+    const handleFilterChange = value => setFilter(value);
+
+    const displayItems = items.filter(item => {
+      if (filter === 'ALL') return true;
+      if (filter === 'TODO') return !item.done;
+      if (filter === 'DONE') return item.done;
+    });
+
     return (
       <div className='panel'>
         <div className='panel-heading'>
           React Todo
         </div>
 
-        {items.map(item => (
-          <label className='panel-block'>
-            <input type='checkbox' />
-            {item.text}
-          </label>
+        <Input onAdd={handleAdd} />
+
+        <Filter
+          onChange={handleFilterChange}
+          value={filter}
+        />
+
+        {displayItems.map(item => (
+          <TodoItem
+            key={item.key}
+            item={item}
+            onCheck={handleCheck}
+          />
         ))}
         <div className='panel-block'>
-          {items.length} items
+          {displayItems.length} items.
         </div>
       </div>
     );
+  }
+
+  function Input() {
+    const [text, setText] = React.useState('');
+
+    const handleChange = e => setText(e.target.value);
+
+    const handleKeyDown = e => {
+      if (e.key === 'Enter') {
+        onAdd(text);
+        setText('');
+      }
+    };
+
+    return (
+      <div className='panel-block'>
+        <input
+          className='input'
+          type='text'
+          placeholder='Enter to add'
+          value={text}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+    );
+  }
+
+  function Filter({ value, onChange }) {
+    const handleClick = (key, e) => {
+      e.preventDefault();
+      onChange(key);
+    }
+
+    return (
+      <div className='panel-tabs'>
+        <a
+          href='#'
+          onClick={handleClick.bind(null, 'ALL')}
+          className={classNames({ 'is-active': value === 'ALL'})}
+        >
+          ALL
+        </a>
+        <a
+          href='#'
+          onClick={handleClick.bind(null, 'TODO')}
+          className={classNames({ 'is-active': value === 'TODO'})}
+        >
+          TODO
+        </a>
+        <a
+          href='#'
+          onClick={handleClick.bind(null, 'DONE')}
+          className={classNames({ 'is-active': value === 'DONE'})}
+        >
+          DONE
+        </a>
+      </div>
+    )
   }
 
   return (
@@ -44,7 +158,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.2/css/bulma.min.css" />
       </Head>
-      <main className={styles.main}>
+      <main className='is-fluid'>
         <Todo />
       </main>
     </>
